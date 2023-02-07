3dc0fac0aa1a84d36c0f08aedc8817bb62e355d9
unknown <sharmadharmesh1998@gmail.com>
2023-02-07 23:03:31 +0530

    Side bar updated

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./BodyStyles.css";
+import {ArrowDown} from "react-feather";
 
+const List = {
+  basicInfo: "Basic Info",
+  workExp: "Work Experience",
+  project: "Projects",
+  education: "Education",
+  achievements: "Achievements",
+  summary: "Summary",
+  other: "Other",
+};
 
 function Body() {
+
+  const[activeclass, setActiveClass] = useState(Object.key(List)[0]);
+
   return (
     <>
        <section className="body-section">
             <div className="body-container">
-                hi
+                <div className="main-body-div">
+                  <div className="from-div">Form Div</div>
+                  <div className="option-div">
+
+                    {Object.keys(List)?.map((key)=>{
+                      return<button className={`"side-btn"`} key={key} onClick={()=>setActiveClass(key)}>{List[key]}</button>
+                    })}
+                    <button className="colour-btn"><input className="color-btn" type="color"/>Choose Color</button>
+                    <button className="download-btn">Download<ArrowDown/></button>
+                  </div>
+                </div>
             </div>
        </section>
     </>
